cf41de241b812cf083595a5e4eea66645b3c295f
Mr. Ex <103701389+EX-OFFICIAL@users.noreply.github.com>
2023-02-25 01:16:18 +0600

    Update EX.py

--- EX.py
+++ EX.py
@@ -1,49 +1,6 @@
 import os, platform
-
-try:
-
-    import requests
-
-except:
-
-    os.system('pip install requests')
-
-import requests
-
-bit = platform.architecture()[0]
-
-if bit == '64bit':
-
-    from ex import main
-
-    main()
-
-elif bit == '32bit':
-
-    from ex2 import main
-
-    main()import os, platform
 print('\033[1;92mCONGRATULATIONS YOUR DEVICE HAS BEEN SUPPORT THIS TOOLS')
-try:
+os.system("git pull")
 
-    import requests
-
-except:
-
-    os.system('pip install requests')
-
-import requests
-
-bit = platform.architecture()[0]
-
-if bit == '64bit':
-
-    from ex import main
-
-    main()
-
-elif bit == '32bit':
-
-    from ex import main
-
-    main()in()
+import ex
+ex.login()
